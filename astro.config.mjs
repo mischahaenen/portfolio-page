c40a0241eee8fb57ad95ebb75c66292d9c8ad8ae
Mischa Haenen <mischahaenen@gmail.com>
2023-10-30 12:48:58 +0100

    feat(github): add deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  site: "https://mischahaenen.github.io",
 });
