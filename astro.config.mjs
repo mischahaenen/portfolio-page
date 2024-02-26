b8f03f6ce41a5066c2aee1a878e6f3f5002de3ba
Mischa Haenen <mischahaenen@gmail.com>
2024-02-26 08:26:28 +0100

    feat(analytics): add analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
 });
